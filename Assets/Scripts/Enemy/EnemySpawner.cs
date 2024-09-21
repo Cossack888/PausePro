576416de6382231e32e3050ba888ff3d4f529e04
Cossack888 <67826646+Cossack888@users.noreply.github.com>
2024-09-22 01:19:46 +0200

    Update Enemy Spawner

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,4 +1,5 @@
 
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,25 +8,40 @@ public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public int numberOfEnemies = 10;
+    public int maxEnemies = 50;
     public float spawnRadius = 10f;
     public float navMeshSampleDistance = 1.0f;
+    public float refreshTime = 60f;
     public Transform spawnCenter;
     List<GameObject> enemies = new List<GameObject>();
     void Start()
     {
-        SpawnEnemies();
+        SpawnEnemies(numberOfEnemies);
+        StartCoroutine(RefreshEnemies(refreshTime));
     }
 
-    void SpawnEnemies()
+    void SpawnEnemies(int numberOfEnemies)
     {
         for (int i = 0; i < numberOfEnemies; i++)
         {
             Vector3 spawnLocation = GetRandomNavMeshPosition(spawnCenter.position, spawnRadius);
             if (spawnLocation != Vector3.zero)
             {
-                Instantiate(enemyPrefab, spawnLocation, Quaternion.identity);
+                GameObject enemy = Instantiate(enemyPrefab, spawnLocation, Quaternion.identity);
+                enemies.Add(enemy);
             }
         }
+    }
+
+    IEnumerator RefreshEnemies(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        if (enemies.Count < maxEnemies)
+        {
+            SpawnEnemies(Random.Range(1, numberOfEnemies));
+            StartCoroutine(RefreshEnemies(waitTime));
+        }
+
     }
 
     public Vector3 GetRandomNavMeshPosition(Vector3 center, float radius)
